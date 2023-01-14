8b29029080d9118db3c845ec9ed6b49e39c1ea99
Eric.Meyer <eric.meyer2019@gmail.com>
2023-01-14 08:34:26 -0600

    Bonus Problem Complete

--- xtra_p1.py
+++ xtra_p1.py
@@ -7,12 +7,12 @@ import random
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+name = "Mr. Extra Creddy"
 
 # Fix the code below to print the name using an f-string
 
 print()
-print("Hello, I'm {name}, your gamebot.")
+print(f"Hello, I'm {name}, your gamebot.")
 print("Let's play an animal guessing game!")
 print("There are 3 animals: wolf, eagle, snake (a Python of course).")
 print("The wolf scares the eagle.")
@@ -26,7 +26,7 @@ print()
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
 
-user_choice = "wolf"
+user_choice = input("Choose 'wolf', 'eagle', or 'snake': ")
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -37,14 +37,24 @@ print(f"You said {user_choice}.")
 print(f"I said {buddy_choice}.")
 print()
 
-
 # Now we need to compare the choices and determine the winner
 # Complete the logic to
 # compare the choices and print who won
 # In Python, indentation is important!
 if user_choice == buddy_choice:
     print("We tied!")
-
+elif user_choice == 'wolf' and buddy_choice == 'eagle':
+    print("You win! Nice job.")
+elif user_choice == 'wolf' and buddy_choice == 'snake':
+    print("I win! Nice try.")
+elif user_choice == 'eagle' and buddy_choice == 'wolf':
+    print("I win! Nice try.")
+elif user_choice == 'eagle' and buddy_choice == 'snake':
+    print("You win! Nice job.")
+elif user_choice == 'snake' and buddy_choice == 'wolf':
+    print("You win! Nice job.")
+elif user_choice == 'snake' and buddy_choice == 'eagle':
+    print("I win! Nice try.")
 
 # When you finish,
 # right-click on the code and select "Format Document"
@@ -53,9 +63,22 @@ if user_choice == buddy_choice:
 # Copy the output from the terminal and paste it into the
 # docstring comment below.
 # --------------------------------------------------------------------
+
 """
 
+Hello, I'm Mr. Extra Creddy, your gamebot.
+Let's play an animal guessing game!
+There are 3 animals: wolf, eagle, snake (a Python of course).
+The wolf scares the eagle.
+The eagle grabs the snake.
+The snake bites the wolf.
+I'll pick one and you pick one and we'll see who wins.
 
+Choose 'wolf', 'eagle', or 'snake': wolf
 
+You said wolf.
+I said eagle.
+
+You win! Nice job.
 
 """
